e7cf0b948d63a70b6cd1dbbbb8a1becd4db17df8
MJ <47497641+MJtuks@users.noreply.github.com>
2019-11-14 13:49:03 +0200

    Plotting code completed
    Added code to :
    + extract the filenames from the command line arguments
    + plot the two input files' data on top of one another
    + name the axis and title of the plot
    + save the plot to a file to the file name that was specified in the command line arguments.

--- plot.py
+++ plot.py
@@ -1,1 +1,35 @@
-import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys
+
+inputFile1 = str(sys.argv[1])
+inputFile2 = str(sys.argv[2])
+outputFile = str(sys.argv[3])
+
+pandaDataFrame = pd.read_csv(inputFile1)
+seconds = list(pandaDataFrame['x-axis'])
+voltages = list(pandaDataFrame['LIN'])
+
+seconds.remove(seconds[0])
+voltages.remove(voltages[0])
+
+seconds = list(map(float, seconds))
+voltages = list(map(float, voltages))
+
+plt.plot(seconds, voltages, color='y')
+
+df = pd.read_csv(inputFile2)
+seconds = list(df['x-axis'])
+voltages = list(df['2'])
+
+seconds.remove(seconds[0])
+voltages.remove(voltages[0])
+
+seconds = list(map(float, seconds))
+voltages = list(map(float, voltages))
+
+plt.plot(seconds, voltages, color='r')
+plt.xlabel("Time (s)")
+plt.ylabel("Voltage (V)")
+plt.title("Transmitted and Received Ultrasonic Signal")
+plt.savefig(outputFile, format='pdf')
